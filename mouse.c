25c57bda27b12cf96b3985738a55e6fa894dab42
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-11-03 00:35:14 +0200

    Refactoring

--- mouse.c
+++ mouse.c
@@ -7,8 +7,6 @@
 #include "msg.h"
 #include "kbd.h"
 
-
-
 static struct spinlock mouse_lock;
 static struct {
 	int x_sgn, y_sgn, x_mov, y_mov;
@@ -16,48 +14,51 @@ static struct {
 } packet;
 static int count;
 
-void mouse_wait(uchar type) {
+void mouse_wait_read() {
 	uint time_out = 100000;
-	if (type == 0) {
-		while (--time_out) {
-			if ((inb(KBSTATP) & 1) == 1) {
-				return;
-			}
-		}
-	} else {
-		while (--time_out) {
-			if ((inb(KBSTATP) & 2) == 0)
-				return;
+	while (--time_out) {
+		if ((inb(KBSTATP) & 1) == 1) {
+			return;
 		}
 	}
 }
 
+void mouse_wait_write() {
+	uint time_out = 100000;
+	while (--time_out) {
+		if ((inb(KBSTATP) & 2) == 0)
+			return;
+	}
+}
+
+
+
 uint mouse_read() {
-	mouse_wait(0);
+	mouse_wait_read();
 	return inb(KBDATAP);
 }
 
 void mouse_write(uchar word) {
-	mouse_wait(1);
+	mouse_wait_write();
 	outb(KBSTATP, 0xd4);
-	mouse_wait(1);
+	mouse_wait_write();
 	outb(KBDATAP, word);
 }
 
 void mouse_init(void) {
 	uchar status_temp;
 
-	mouse_wait(1);
+	mouse_wait_write();
 	outb(KBSTATP, 0xa8);
 
-	mouse_wait(0);
+	mouse_wait_read();
 
 	status_temp = (inb(KBDATAP) | 2);
 
-	mouse_wait(0);
+	mouse_wait_read();
 	outb(KBSTATP, 0x60);
 
-	mouse_wait(1);
+	mouse_wait_write();
 	outb(KBDATAP, status_temp);
 
 	mouse_write(0xf6);
@@ -119,9 +120,9 @@ void mouseintr(void) {
 		} else {
 			count = 0;
 		}
-	} else if(count == 2){
+	} else if (count == 2) {
 		packet.x_mov = data;
-	} else if(count==3){
+	} else if (count == 3) {
 		packet.y_mov = data;
 		count = 0;
 		gen_mouse_message();
