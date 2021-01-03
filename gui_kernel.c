8cdcb90656e521ff636d5370b91ea471c11cfc06
Denis Ivanenko <ivanenko@ucu.edu.ua>
2021-01-03 17:01:29 +0200

    Added assets

---
+++ gui_kernel.c
@@ -0,0 +1,11 @@
+//
+// Created by Denis Ivanenko on 02.01.2021.
+//
+
+
+int focus = -1;
+struct
+
+
+
+
---
+++ gui_kernel.h
@@ -0,0 +1,8 @@
+//
+// Created by Denis Ivanenko on 03.01.2021.
+//
+
+#ifndef XV6_GUI__GUI_KERNEL_H_
+#define XV6_GUI__GUI_KERNEL_H_
+
+#endif //XV6_GUI__GUI_KERNEL_H_
